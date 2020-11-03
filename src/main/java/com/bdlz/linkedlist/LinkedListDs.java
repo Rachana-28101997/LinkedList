7bc299eb1956647748980172efe7e1b59cdb7926
Rachana <rachanarrachu28@gmail.com>
2020-11-04 01:11:41 +0530

    Added : The elements appended at the last

--- src/main/java/com/bdlz/linkedlist/LinkedListDs.java
+++ src/main/java/com/bdlz/linkedlist/LinkedListDs.java
@@ -19,4 +19,17 @@ public class LinkedListDs<K> {
 			this.head.setNext(tempNode);
 		}
 	}
+
+	public void append(InterfaceNode<K> myNode) {
+		if(this.head == null) {
+			this.head = myNode;
+		}
+		if(this.tail == null) {
+			this.tail = myNode;
+		}else {
+			this.tail.setNext(myNode);
+			this.tail = myNode;
+		}
+	}
+	
 }
--- src/test/java/com/bdlz/linkedlist/TestLinkedList.java
+++ src/test/java/com/bdlz/linkedlist/TestLinkedList.java
@@ -6,31 +6,43 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestLinkedList {
-	
 
+	@Test
+	public void ThreeNumbersSimpleLinkedList() {
+		ImplementNode<Integer> firstNode = new ImplementNode<Integer>(56);
+		ImplementNode<Integer> secondNode = new ImplementNode<Integer>(30);
+		ImplementNode<Integer> thirdNode = new ImplementNode<Integer>(70);
+		firstNode.setNext(secondNode);
+		secondNode.setNext(thirdNode);
+		boolean result = firstNode.getNext().equals(secondNode) && secondNode.getNext().equals(thirdNode);
+		Assert.assertTrue(result);
+	}
 
+	@Test
+	public void whenAddedToLinkedListShouldBeAddedAtTheTop() {
+		ImplementNode<Integer> firstNode = new ImplementNode<Integer>(70);
+		ImplementNode<Integer> secondNode = new ImplementNode<Integer>(30);
+		ImplementNode<Integer> thirdNode = new ImplementNode<Integer>(56);
+		LinkedListDs lst = new LinkedListDs();
+		lst.add(firstNode);
+		lst.add(secondNode);
+		lst.add(thirdNode);
+		boolean result = LinkedListDs.head.equals(thirdNode) && LinkedListDs.head.getNext().equals(secondNode)
+				&& LinkedListDs.tail.equals(firstNode);
+		Assert.assertTrue(result);
+	}
 
-		@Test
-		public void ThreeNumbersSimpleLinkedList() {
-			ImplementNode<Integer> firstNode = new ImplementNode<Integer>(56);
-			ImplementNode<Integer> secondNode = new ImplementNode<Integer>(30);
-			ImplementNode<Integer> thirdNode = new ImplementNode<Integer>(70);
-			firstNode.setNext(secondNode);
-			secondNode.setNext(thirdNode);
-			boolean result = firstNode.getNext().equals(secondNode) && secondNode.getNext().equals(thirdNode);
-			Assert.assertTrue(result);
-		}
-		@Test
-		public void given3NumbersWhenAddedToLinkedListShouldBeAddedAtTheTop() {
-			ImplementNode<Integer> firstNode = new ImplementNode<Integer>(70);
-			ImplementNode<Integer> secondNode = new ImplementNode<Integer>(30);
-			ImplementNode<Integer> thirdNode = new ImplementNode<Integer>(56);
-			LinkedListDs lst = new LinkedListDs();
-			lst.add(firstNode);
-			lst.add(secondNode);
-			lst.add(thirdNode);
-			boolean result = LinkedListDs.head.equals(thirdNode) && LinkedListDs.head.getNext().equals(secondNode)
-					&& LinkedListDs.tail.equals(firstNode);
-			Assert.assertTrue(result);
-		}
+	@Test
+	public void whenAppendedToLinkedListShouldBeAddedAtTheLast() {
+		ImplementNode<Integer> firstNode = new ImplementNode<Integer>(56);
+		ImplementNode<Integer> secondNode = new ImplementNode<Integer>(30);
+		ImplementNode<Integer> thirdNode = new ImplementNode<Integer>(70);
+		LinkedListDs lst = new LinkedListDs();
+		lst.add(firstNode);
+		lst.append(secondNode);
+		lst.append(thirdNode);
+		boolean result = LinkedListDs.head.equals(firstNode) && LinkedListDs.head.getNext().equals(secondNode)
+				&& LinkedListDs.tail.equals(thirdNode);
+		Assert.assertTrue(result);
+	}
 }
